1394e5e60db4bd1ac4278cb5259e1625e919b7e8
furp-oss <bockinsup@gmail.com>
2025-01-05 07:47:56 +0300

    Create auto.ts

---
+++ common/auto.ts
@@ -0,0 +1,24 @@
+export abstract class ToHTML {
+	abstract getCode(): string;
+}
+
+interface Customization {
+	command: string;
+	text: {
+		solo: string;
+		group: string;
+		help: string;
+		button?: string;
+	};
+
+	getRandom: () => ToHTML;
+	article: {
+		title: string;
+		description: string;
+		image: { url: string; width?: number; height?: number };
+	};
+
+	base?: { sendCommand?: string; orUseButton?: string };
+}
+
+export declare function auto(id: BotIds, config: Customization): unknown;
