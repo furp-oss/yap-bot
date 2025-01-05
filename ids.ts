c109bb2bac08a945e4c3f44a80448128be5bd5f3
furp-oss <bockinsup@gmail.com>
2025-01-05 07:44:18 +0300

    Update ids.ts

--- ids.ts
+++ ids.ts
@@ -3,3 +3,13 @@ export enum BotIds {
 	PROG = 44,
   // ...
 }
+
+
+
+export const usernames: Record<BotIds, string> = Object.freeze({
+	[BotIds.PROG]: 'tvoy_yap_bot'
+});
+
+export const allBotIds: readonly BotIds[] = Object.keys(BotIds)
+	.map((value) => parseInt(value))
+	.filter((key) => !isNaN(key)) as [] as BotIds[];
